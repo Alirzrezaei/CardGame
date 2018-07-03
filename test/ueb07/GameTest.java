96a888b0b78b170a0f5c6fb9d0a0f3f5a389f1d4
ite102770 <ite102770@383cc300-1c0e-49e2-a6cb-a3da9cc689f7>
2018-07-03 14:00:31 +0000

    new Commit

--- test/ueb07/GameTest.java
+++ test/ueb07/GameTest.java
@@ -54,11 +54,12 @@ public class GameTest {
 
     @Test
     public void testNextPlayerNormalSituation() {
-        Card[][] packs = new Card[][]{{Card.EIGHT_SPADES, Card.KING_HEARTS, Card.ACE_DIAMONDS}, {Card.NINE_DIAMONDS, Card.QUEEN_CLUBS,
+        Card[][] packs = new Card[][]{{Card.EIGHT_SPADES, Card.KING_HEARTS, Card.ACE_DIAMONDS}, 
+            {Card.NINE_DIAMONDS, Card.QUEEN_CLUBS,
             Card.KING_DIAMONDS}, {Card.TEN_DIAMONDS, Card.TEN_CLUBS, Card.ACE_CLUBS}};
         Game gameTest = new Game(packs, "rcr");
         gameTest.doTurn(gameTest.getPlayers()[0], gameTest.getCardsToTop());
-        System.out.println(gameTest.getPlayers()[0].getPack().toString());
+        System.out.println(gameTest.getPlayers()[0].toString());
         int nextPlayer = gameTest.nextPlayer(0, gameTest.getCardsToTop(), gameTest.getCountCantLay());
         assertEquals(1, nextPlayer);
         assertEquals(0, gameTest.getCountCantLay());
@@ -126,8 +127,19 @@ public class GameTest {
     public void tesToString() {
         Card[][] packs = new Card[][]{{Card.ACE_CLUBS, Card.ACE_DIAMONDS, Card.ACE_SPADES},
         {Card.SEVEN_CLUBS, Card.SEVEN_DIAMONDS, Card.SEVEN_HEARTS}, 
-        {Card.TEN_CLUBS, Card.TEN_DIAMONDS, Card.QUEEN_CLUBS}};
+        {Card.TEN_CLUBS, Card.TEN_DIAMONDS, Card.TEN_DIAMONDS}};
         Game gameTest = new Game(packs, "ccr");
        assertEquals("C0: ACE_CLUBS, ACE_DIAMONDS, ACE_SPADES", gameTest.getPlayers()[0].toString());
+       assertEquals("C1: SEVEN_CLUBS, SEVEN_DIAMONDS, SEVEN_HEARTS", gameTest.getPlayers()[1].toString());
+       assertEquals("R2: TEN_CLUBS, TEN_DIAMONDS, TEN_DIAMONDS", gameTest.getPlayers()[2].toString());
+    }
+    @Test
+    public void testDealCards() {
+        
+        Game gameTest = new Game(3);
+        gameTest.dealCards();
+        System.out.println(gameTest.getPlayers()[0].toString());
+        System.out.println(gameTest.getPlayers()[1].toString());
+        System.out.println(gameTest.getPlayers()[2].toString());
     }
 }
