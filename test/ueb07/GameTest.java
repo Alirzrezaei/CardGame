c0f60bd4e2f3c1d632c55fa91b4f45c84795707f
ite102770 <ite102770@383cc300-1c0e-49e2-a6cb-a3da9cc689f7>
2018-07-03 09:05:14 +0000

    update test cases done todos

--- test/ueb07/GameTest.java
+++ test/ueb07/GameTest.java
@@ -1,73 +1,73 @@
-
 package ueb07;
 
 import org.junit.Test;
 import static org.junit.Assert.*;
 import ueb07.Game.*;
 import ueb07.cards.Card;
+
 /**
  *
  * @author ite102770
  */
 public class GameTest {
-    
+
     public GameTest() {
     }
 
     @Test
     public void testDoTurnSucceded() {
-        Card[][] packs = new Card[][] {{Card.EIGHT_SPADES, Card.KING_HEARTS, Card.ACE_DIAMONDS},
-            {Card.NINE_DIAMONDS,Card.QUEEN_CLUBS,Card.KING_DIAMONDS},
-            {Card.TEN_DIAMONDS,Card.TEN_CLUBS,Card.ACE_CLUBS}}; 
-        Game gameTest =  new Game(packs, "rcr");
+        Card[][] packs = new Card[][]{{Card.EIGHT_SPADES, Card.KING_HEARTS, Card.ACE_DIAMONDS},
+        {Card.NINE_DIAMONDS, Card.QUEEN_CLUBS, Card.KING_DIAMONDS},
+        {Card.TEN_DIAMONDS, Card.TEN_CLUBS, Card.ACE_CLUBS}};
+        Game gameTest = new Game(packs, "rcr");
         gameTest.doTurn(gameTest.getPlayers()[0], gameTest.getCardsToTop());
         assertArrayEquals(new Card[]{Card.EIGHT_SPADES}, gameTest.getCardsToTop());
         assertArrayEquals(new Card[]{Card.KING_HEARTS, Card.ACE_DIAMONDS}, gameTest.getPlayers()[0].getPack().toArray());
     }
-    
+
     @Test
     public void testDoTurnFailed() {
-        Card[][] packs = new Card[][] {{Card.EIGHT_SPADES, Card.KING_HEARTS, Card.ACE_DIAMONDS},
-            {Card.SEVEN_CLUBS,Card.SEVEN_DIAMONDS,Card.SEVEN_HEARTS},
-            {Card.TEN_CLUBS,Card.TEN_DIAMONDS,Card.ACE_CLUBS}}; 
-        Game gameTest =  new Game(packs, "rcr");
-        gameTest.doTurn(gameTest.getPlayers()[0], gameTest.getCardsToTop());
+        Card[][] packs = new Card[][]{{Card.EIGHT_SPADES, Card.KING_HEARTS, Card.ACE_DIAMONDS},
+        {Card.SEVEN_CLUBS, Card.SEVEN_DIAMONDS, Card.SEVEN_HEARTS},
+        {Card.TEN_CLUBS, Card.TEN_DIAMONDS, Card.ACE_CLUBS}};
+        Game gameTest = new Game(packs, "rcr");
+        gameTest.nextPlayer(0, gameTest.getCardsToTop(), gameTest.getCountCantLay());
+
+        gameTest.nextPlayer(1, gameTest.getCardsToTop(), gameTest.getCountCantLay());
         assertArrayEquals(new Card[]{Card.EIGHT_SPADES}, gameTest.getCardsToTop());
-        gameTest.doTurn(gameTest.getPlayers()[1], gameTest.getCardsToTop());
         assertEquals(1, gameTest.getCountCantLay());
-        
-        assertArrayEquals(new Card[]{Card.SEVEN_CLUBS,Card.SEVEN_DIAMONDS,Card.SEVEN_HEARTS}, gameTest.getPlayers()[1].getPack().toArray());
+        assertArrayEquals(new Card[]{Card.SEVEN_CLUBS, Card.SEVEN_DIAMONDS, Card.SEVEN_HEARTS}, gameTest.getPlayers()[1].getPack().toArray());
     }
-    
+
     @Test(expected = AssertionError.class)
     public void testDoTurnPlayerHasNoLeftCards() {
-        Card[][] packs = new Card[][] {{Card.EIGHT_SPADES, Card.KING_HEARTS, Card.ACE_DIAMONDS},
-            {},{Card.TEN_DIAMONDS,Card.TEN_CLUBS,Card.ACE_CLUBS}}; 
-        Game gameTest =  new Game(packs, "rcr");
+        Card[][] packs = new Card[][]{{Card.EIGHT_SPADES, Card.KING_HEARTS, Card.ACE_DIAMONDS},
+        {}, {Card.TEN_DIAMONDS, Card.TEN_CLUBS, Card.ACE_CLUBS}};
+        Game gameTest = new Game(packs, "rcr");
         gameTest.doTurn(gameTest.getPlayers()[0], gameTest.getCardsToTop());
         gameTest.doTurn(gameTest.getPlayers()[1], gameTest.getCardsToTop());
         assertArrayEquals(new Card[]{Card.EIGHT_SPADES}, gameTest.getCardsToTop());
         assertArrayEquals(new Card[]{}, gameTest.getPlayers()[1].getPack().toArray());
     }
-    
+
     @Test
     public void testNextPlayerNormalSituation() {
-        Card[][] packs = new Card[][] {{Card.EIGHT_SPADES, Card.KING_HEARTS, Card.ACE_DIAMONDS},{Card.NINE_DIAMONDS,Card.QUEEN_CLUBS,
-            Card.KING_DIAMONDS},{Card.TEN_DIAMONDS,Card.TEN_CLUBS,Card.ACE_CLUBS}}; 
-        Game gameTest =  new Game(packs, "rcr");
+        Card[][] packs = new Card[][]{{Card.EIGHT_SPADES, Card.KING_HEARTS, Card.ACE_DIAMONDS}, {Card.NINE_DIAMONDS, Card.QUEEN_CLUBS,
+            Card.KING_DIAMONDS}, {Card.TEN_DIAMONDS, Card.TEN_CLUBS, Card.ACE_CLUBS}};
+        Game gameTest = new Game(packs, "rcr");
         gameTest.doTurn(gameTest.getPlayers()[0], gameTest.getCardsToTop());
         int nextPlayer = gameTest.nextPlayer(0, gameTest.getCardsToTop(), gameTest.getCountCantLay());
         assertEquals(1, nextPlayer);
         assertEquals(0, gameTest.getCountCantLay());
         assertArrayEquals(new Card[]{Card.NINE_DIAMONDS}, gameTest.getCardsToTop());
-        assertArrayEquals(new Card[]{Card.QUEEN_CLUBS,Card.KING_DIAMONDS}, gameTest.getPlayers()[1].getPack().toArray());
+        assertArrayEquals(new Card[]{Card.QUEEN_CLUBS, Card.KING_DIAMONDS}, gameTest.getPlayers()[1].getPack().toArray());
     }
-    
+
     @Test
     public void testNextPlayerOnePlayerHasToSkip() {
-        Card[][] packs = new Card[][] {{Card.EIGHT_SPADES, Card.KING_HEARTS, Card.ACE_DIAMONDS},
-            {Card.SEVEN_CLUBS,Card.SEVEN_DIAMONDS,Card.SEVEN_HEARTS},{Card.TEN_CLUBS, Card.TEN_DIAMONDS, Card.ACE_CLUBS}}; 
-        Game gameTest =  new Game(packs, "rcr");
+        Card[][] packs = new Card[][]{{Card.EIGHT_SPADES, Card.KING_HEARTS, Card.ACE_DIAMONDS},
+        {Card.SEVEN_CLUBS, Card.SEVEN_DIAMONDS, Card.SEVEN_HEARTS}, {Card.TEN_CLUBS, Card.TEN_DIAMONDS, Card.ACE_CLUBS}};
+        Game gameTest = new Game(packs, "rcr");
         gameTest.doTurn(gameTest.getPlayers()[0], gameTest.getCardsToTop());
         int nextPlayer = gameTest.nextPlayer(0, gameTest.getCardsToTop(), gameTest.getCountCantLay());
         assertEquals(2, nextPlayer);
@@ -75,12 +75,12 @@ public class GameTest {
         assertArrayEquals(new Card[]{Card.TEN_CLUBS, Card.TEN_DIAMONDS}, gameTest.getCardsToTop());
         assertArrayEquals(new Card[]{Card.ACE_CLUBS}, gameTest.getPlayers()[2].getPack().toArray());
     }
-    
+
     @Test
     public void testNextPlayerAllPlayersSkiped() {
-        Card[][] packs = new Card[][] {{Card.QUEEN_DIAMONDS, Card.KING_HEARTS, Card.ACE_DIAMONDS},
-            {Card.SEVEN_CLUBS,Card.SEVEN_DIAMONDS,Card.SEVEN_HEARTS},{Card.TEN_CLUBS, Card.TEN_DIAMONDS, Card.QUEEN_CLUBS}}; 
-        Game gameTest =  new Game(packs, "rcr");
+        Card[][] packs = new Card[][]{{Card.QUEEN_DIAMONDS, Card.KING_HEARTS, Card.ACE_DIAMONDS},
+        {Card.SEVEN_CLUBS, Card.SEVEN_DIAMONDS, Card.SEVEN_HEARTS}, {Card.TEN_CLUBS, Card.TEN_DIAMONDS, Card.QUEEN_CLUBS}};
+        Game gameTest = new Game(packs, "rcr");
         gameTest.doTurn(gameTest.getPlayers()[0], gameTest.getCardsToTop());
         int nextPlayer = gameTest.nextPlayer(0, gameTest.getCardsToTop(), gameTest.getCountCantLay());
         assertEquals(0, nextPlayer);
@@ -88,12 +88,12 @@ public class GameTest {
         assertArrayEquals(new Card[]{Card.KING_HEARTS}, gameTest.getCardsToTop());
         assertArrayEquals(new Card[]{Card.ACE_DIAMONDS}, gameTest.getPlayers()[0].getPack().toArray());
     }
-    
+
     @Test
     public void testNextPlayerAcePlacedCautiousPlayer() {
-        Card[][] packs = new Card[][] {{Card.ACE_CLUBS, Card.ACE_DIAMONDS, Card.ACE_SPADES},
-            {Card.SEVEN_CLUBS,Card.SEVEN_DIAMONDS,Card.SEVEN_HEARTS},{Card.TEN_CLUBS, Card.TEN_DIAMONDS, Card.QUEEN_CLUBS}}; 
-        Game gameTest =  new Game(packs, "ccr");
+        Card[][] packs = new Card[][]{{Card.ACE_CLUBS, Card.ACE_DIAMONDS, Card.ACE_SPADES},
+        {Card.SEVEN_CLUBS, Card.SEVEN_DIAMONDS, Card.SEVEN_HEARTS}, {Card.TEN_CLUBS, Card.TEN_DIAMONDS, Card.QUEEN_CLUBS}};
+        Game gameTest = new Game(packs, "ccr");
         gameTest.doTurn(gameTest.getPlayers()[0], gameTest.getCardsToTop());
         int nextPlayer = gameTest.nextPlayer(0, gameTest.getCardsToTop(), gameTest.getCountCantLay());
         assertEquals(0, nextPlayer);
@@ -101,12 +101,12 @@ public class GameTest {
         assertArrayEquals(new Card[]{Card.ACE_SPADES}, gameTest.getCardsToTop());
         assertArrayEquals(new Card[]{}, gameTest.getPlayers()[0].getPack().toArray());
     }
-    
+
     @Test
     public void testNextPlayerAcePlaced() {
-        Card[][] packs = new Card[][] {{Card.JACK_DIAMONDS, Card.QUEEN_CLUBS, Card.ACE_SPADES},
-            {Card.SEVEN_CLUBS,Card.SEVEN_DIAMONDS,Card.SEVEN_HEARTS},{Card.TEN_CLUBS, Card.TEN_DIAMONDS, Card.KING_DIAMONDS}}; 
-        Game gameTest =  new Game(packs, "crc");
+        Card[][] packs = new Card[][]{{Card.JACK_DIAMONDS, Card.QUEEN_CLUBS, Card.ACE_SPADES},
+        {Card.SEVEN_CLUBS, Card.SEVEN_DIAMONDS, Card.SEVEN_HEARTS}, {Card.TEN_CLUBS, Card.TEN_DIAMONDS, Card.KING_DIAMONDS}};
+        Game gameTest = new Game(packs, "crc");
         gameTest.doTurn(gameTest.getPlayers()[0], gameTest.getCardsToTop());
         int nextPlayer = gameTest.nextPlayer(0, gameTest.getCardsToTop(), gameTest.getCountCantLay());
         assertEquals(2, nextPlayer);
@@ -119,5 +119,5 @@ public class GameTest {
         assertEquals(0, nextPlayer3);
         assertArrayEquals(new Card[]{Card.QUEEN_CLUBS}, gameTest.getCardsToTop());
     }
-    
+
 }
